8739bcdb09b0c717c39c780a58d2b9d927b8b35d
Aaron Rivas <aarivas793@gmail.com>
2023-12-15 11:16:48 -0800

    Completed Amazon Best Sellers, React Fundamentals

---
+++ src/Book.js
@@ -0,0 +1,12 @@
+const Book = ({ title, img, author, index }) => {
+  return (
+    <article className="book">
+      <span className="number"># {index + 1}</span>
+      <img src={img} alt={title} />
+      <h2>{title}</h2>
+      <h4>{author.toUpperCase()}</h4>
+    </article>
+  );
+};
+
+export default Book;
---
+++ src/books.js
@@ -0,0 +1,24 @@
+import img1 from './images/img-1.jpg';
+import img2 from './images/img-2.jpeg';
+import img3 from './images/img-3.jpg';
+
+export const books = [
+  {
+    title: 'Interesting Facts for Curious Minds',
+    author: 'Jordan Moore',
+    img: img1,
+    id: 1,
+  },
+  {
+    title: 'Oath and Honor: A Memoir and a Warning',
+    author: 'Liz Cheney',
+    img: img2,
+    id: 2,
+  },
+  {
+    title: 'Fourth Wing (The Empyrean, 1)',
+    author: 'Rebecca Yarros',
+    img: img3,
+    id: 3,
+  },
+];
